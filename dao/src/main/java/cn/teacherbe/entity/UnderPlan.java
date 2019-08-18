ab03adaefa7b66af0444ab3f7a79ef321f53a4e2
woshibixiangfei <bixiangfei02>
2019-08-19 03:01:58 +0800

    1

---
+++ dao/src/main/java/cn/teacherbe/entity/UnderPlan.java
@@ -0,0 +1,24 @@
+package cn.teacherbe.entity;
+
+public class UnderPlan {
+
+    private String id;
+
+    private String kua;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKua() {
+        return kua;
+    }
+
+    public void setKua(String kua) {
+        this.kua = kua;
+    }
+}
